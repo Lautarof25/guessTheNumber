22493ed4ed7b82a636fc275f31150e3cc3ddc55f
Lautarof25 <fernandez.lautaro25@gmail.com>
2023-06-11 12:17:06 -0300

    Renderizando

--- source/js/showCharacters.js
+++ source/js/showCharacters.js
@@ -9,20 +9,20 @@ function addCharacter(number) {
     case 1:
       rooting.innerHTML = `
         <p class="bubble speech text-end bg-white rounded p-1">Muy bien, estás muy cerca de conseguirlo!</p>
-        <img src="./source/img/character_1.png" alt="rooting character">
+        <img src="./source/img/character_1.webp" alt="rooting character">
         `
       break;
     case 2:
       rooting.innerHTML =
         `
-        <img src="./source/img/character_2.png" alt="rooting character">
+        <img src="./source/img/character_2.webp" alt="rooting character">
         <p class="bubble speech bg-white rounded p-1">¡Solo falta acomodar los números!</p>
         `
       break;
     case 3:
       rooting.innerHTML =
         `
-        <img src="./source/img/character_2.png" alt="rooting character">
+        <img src="./source/img/character_2.webp" alt="rooting character">
         <p class="bubble speech bg-white rounded p-1"></p>
         `
       break;
