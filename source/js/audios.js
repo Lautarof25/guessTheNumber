ec0f0ff930d24f6f417a710ebca20f56fecf0a32
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-07-01 20:44:17 -0300

    test audio mp3

--- source/js/audios.js
+++ source/js/audios.js
@@ -1,5 +1,5 @@
-const opening = new Audio('../source/audio/opening.ogg')
-const insertNumber = new Audio('../source/audio/addNumber.ogg');
-const winner = new Audio('../source/audio/winGame.ogg');
-const gameOver = new Audio('../source/audio/gameOver.ogg');
-const repeat = new Audio('../source/audio/repeatNumber.ogg');
+const opening = new Audio('../source/audio/opening.mp3')
+const insertNumber = new Audio('../source/audio/addNumber.mp3');
+const winner = new Audio('../source/audio/winGame.mp3');
+const gameOver = new Audio('../source/audio/gameOver.mp3');
+const repeat = new Audio('../source/audio/repeatNumber.mp3');
