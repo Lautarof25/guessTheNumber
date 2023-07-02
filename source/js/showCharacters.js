6ebd3be16d500286dcd2d9581e84b091bebf7014
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-07-02 12:56:37 -0300

    changing character insertion

--- source/js/showCharacters.js
+++ source/js/showCharacters.js
@@ -1,43 +1,46 @@
-const rooting = document.querySelector("#rooting")
-
-function removeCharacter() {
-  rooting.innerHTML = ""
-}
-
-function addCharacter(number) {
-  switch (number) {
-    case 1:
-      rooting.innerHTML = `
-        <p class="bubble speech text-end bg-white rounded p-1 shadow ">Muy bien, estás muy cerca de conseguirlo!</p>
-        <img src="./source/img/character_1.webp" alt="rooting character">
-        `
-      break;
-    case 2:
-      rooting.innerHTML =
-        `
-        <img src="./source/img/character_2.webp" alt="rooting character">
-        <p class="bubble speech bg-white rounded p-1 shadow ">¡Solo falta acomodar los números!</p>
-        `
-      break;
+function templateCharacter(number){
+  const rooting = document.createElement("div")
+  rooting.setAttribute("id","rooting")
+  rooting.setAttribute("class","d-flex index-1 position-absolute bottom-0 start-50 translate-middle-x justify-content-center align-items-center opacity")
+  const p = document.createElement("p")
+  const img = document.createElement("img")
+  const textThree = "Muy bien, estás muy cerca de conseguirlo!"
+  const textFour = "¡Solo falta acomodar los números!"
+  p.setAttribute('class', `bubble speech text-end bg-white rounded p-2 shadow `)
+  if(number == 1){
+    const textP = document.createTextNode(textThree);
+    img.setAttribute("src","./source/img/character_1.webp")
+    p.appendChild(textP)
+    rooting.appendChild(p)
+    rooting.appendChild(img)
+  }else {
+    const textP = document.createTextNode(textFour);
+    img.setAttribute("src","./source/img/character_2.webp")
+    p.appendChild(textP)
+    rooting.appendChild(img)
+    rooting.appendChild(p)
   }
+  document.querySelector("#rows").appendChild(rooting)
 }
 
 function character(num) {
   if (checkOk(num) == digitLimit - 1) {
-    rooting.classList.remove("opacity")
-    rooting.classList.add("appearDisappear")
-    addCharacter(1)
+    templateCharacter(1)
+    document.querySelector("#rooting").classList.remove("opacity")
+    document.querySelector("#rooting").classList.add("appearDisappear")
+    characterSound.play()
     setTimeout(() => {
-      rooting.classList.remove("appearDisappear")
-      removeCharacter()
+      document.querySelector("#rooting").classList.remove("appearDisappear")
+      document.querySelector("#rooting").remove()
     }, 5000);
   } else if (checkOk(num) == digitLimit && checkIndex(num) <= digitLimit-1) {
-    rooting.classList.remove("opacity")
-    rooting.classList.add("appearDisappear")
-    addCharacter(2)
+    templateCharacter(2)
+    document.querySelector("#rooting").classList.remove("opacity")
+    document.querySelector("#rooting").classList.add("appearDisappear")
+    characterSound.play()
     setTimeout(() => {
-      rooting.classList.remove("appearDisappear")
-      removeCharacter()
+      document.querySelector("#rooting").classList.remove("appearDisappear")
+      document.querySelector("#rooting").remove()
     }, 5000);
   }
 }
