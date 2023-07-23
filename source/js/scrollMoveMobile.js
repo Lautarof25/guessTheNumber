1c21f3f086697eb67a22729d627abc15c3952427
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-07-22 21:24:14 -0300

    Refactoring

--- source/js/scrollMoveMobile.js
+++ source/js/scrollMoveMobile.js
@@ -1,6 +1,5 @@
-function goToNewRow(){
+function goToNewRow() {
     const rect = numberGuess.getBoundingClientRect()
-    const y = rect.y +  window.scrollY;
-    window.scrollTo(0,y)
-    console.log(y)
+    const y = rect.y + window.scrollY;
+    setTimeout(window.scrollTo(0, y),100)
 }
--- source/js/showCharacters.js
+++ source/js/showCharacters.js
@@ -1,48 +1,47 @@
-function templateCharacter(number){
+function templateCharacter(number) {
   const rooting = document.createElement("div")
-  rooting.setAttribute("id","rooting")
-  rooting.setAttribute("class","d-flex index-1 position-absolute bottom-25 start-50 translate-middle-x justify-content-center align-items-center opacity")
+  rooting.setAttribute("id", "rooting")
+  rooting.setAttribute("class", "d-flex index-1 position-absolute bottom-25 start-50 translate-middle-x justify-content-center align-items-center opacity")
   const p = document.createElement("p")
   const img = document.createElement("img")
   p.setAttribute('class', `bubble speech text-end bg-white rounded p-2 shadow `)
-  if(number == 4){
+  if (number == 4) {
     const textFour = "¡Solo falta ordenar los números!"
     var textP = document.createTextNode(textFour);
-    img.setAttribute("src","./source/img/character_2.webp")
+    img.setAttribute("src", "./source/img/character_2.webp")
     rooting.appendChild(img)
     rooting.appendChild(p)
-  }else{
-    if(number == 3) {
+  } else {
+    if (number == 3) {
       var textThree = "Muy bien, estás muy cerca de conseguirlo!"
       var textP = document.createTextNode(textThree);
     }
-    else{
+    else {
       var textZero = "¡Sigue adelante!"
       var textP = document.createTextNode(textZero);
-    } 
-    img.setAttribute("src","./source/img/character_1.webp")
+    }
+    img.setAttribute("src", "./source/img/character_1.webp")
     rooting.appendChild(p)
     rooting.appendChild(img)
   }
   p.appendChild(textP)
   document.querySelector("#rows").appendChild(rooting)
+  characterEffect()
 }
 
 function addCharacter(num) {
   const numbersOks = checkOk(num)
-  if (checkOk(num) == digitLimit - 1 && attempts > 1) {
-    templateCharacter(numbersOks)
-    characterEffect()
-  } else if (checkOk(num) == digitLimit && checkIndex(num) <= digitLimit-1 && attempts > 1) {
-    templateCharacter(numbersOks)
-    characterEffect()
-  }else if(checkOk(num) == 0 && attempts > 1){
-    templateCharacter(numbersOks)
-    characterEffect()
+  if(attempts > 1){
+    if(numbersOks == digitLimit && checkIndex(num)<= digitLimit - 2)
+      templateCharacter(numbersOks)
+    else if(numbersOks == digitLimit - 1)
+      templateCharacter(numbersOks)
+    else if(numbersOks == 0)
+      templateCharacter(numbersOks)
   }
 }
 
-function characterEffect(){
+function characterEffect() {
   numberGuess.classList.add("d-none")
   numberGuess.disabled = true
   document.querySelector("#rooting").classList.remove("opacity")
